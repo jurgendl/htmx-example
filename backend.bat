96829e5b77034a77603d8234b6c0f261d23bba17
jurgendl <jurgen.de.landsheer@gmail.com>
2025-02-08 14:27:45 +0100

    htmx example

--- backend.bat
+++ backend.bat
@@ -2,7 +2,7 @@ start http://localhost:8080/
 cd backend
 cd htmx
 call mvnw.cmd install -DskipTests
-java -jar target/htmx-0.0.1-SNAPSHOT.jar
+java -jar target/htmx-backend-0.0.1-SNAPSHOT.jar
 cd ..
 cd ..
 pause
