09a7eb7c825494209d7b779fd200365ef35070d8
jurgendl <jurgen.de.landsheer@gmail.com>
2025-02-08 14:26:40 +0100

    htmx example

---
+++ frontend/htmx/src/htmx-frontend-index.ts
@@ -0,0 +1,6 @@
+import './htmx-frontend.scss'
+import { ViteSeedApp } from './components/htmx-frontend-app';
+
+const app = new ViteSeedApp();
+app.init();
+
--- frontend/htmx/src/vite-seed-index.ts
+++
@@ -1,6 +0,0 @@
-import './vite-seed.scss'
-import { ViteSeedApp } from './components/vite-seed-app';
-
-const app = new ViteSeedApp();
-app.init();
-
