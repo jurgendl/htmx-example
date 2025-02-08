a5fe49e431034786d877ec59cc28eb6b6cc44c4c
jurgendl <jurgen.de.landsheer@gmail.com>
2025-02-08 15:45:08 +0100

    htmx example

--- bundle.bat
+++ bundle.bat
@@ -1,11 +1,14 @@
 cd frontend
 cd htmx
-call npm run build
+call npm.cmd build
 cd ..
 cd ..
+
 cd backend
 cd htmx
 call gradle build
+start http://localhost:8080/
+java -jar build/libs/htmx-backend-0.0.1-SNAPSHOT.jar
 cd ..
 cd ..
 pause
