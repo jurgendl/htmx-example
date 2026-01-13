71f41967798bd9f06d09964203ad4572f5e846e4
jdlandsh <jurgen.delandsheer@ugent.be>
2026-01-13 17:00:54 +0100

    htmx example

--- backend.bat
+++ backend.bat
@@ -1,7 +1,7 @@
-start http://localhost:14825/
 cd backend
 cd htmx
 call gradle build
+start http://localhost:14825/
 java -jar build/libs/htmx-backend-0.0.1-SNAPSHOT.jar
 cd ..
 cd ..
--- frontend.bat
+++ frontend.bat
@@ -1,8 +1,9 @@
-start http://localhost:5173/
 cd frontend
 cd htmx
 call npm install
-call npm run dev
+call npm run build
+start http://localhost:4173/
+call npm run preview
 cd ..
 cd ..
 pause 
